9484bba8cefcfd3cc85123543539bc9af37ddd78
Bryce Campbell <tonyhawk2100@gmail.com>
2021-06-10 18:48:37 -0700

    remove test code.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,5 @@ mod pair;
 mod combination;
 mod scoreboard;
 
-use die::Die;
-
 fn main() {
-    let mut die = Die::new();
-
-    die.roll();
-    die.is_held = true;
-
-    println!("{}", die);
 }
