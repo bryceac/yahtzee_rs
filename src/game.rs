253987bd45ee31703dbd36edef99b88ba8211989
Bryce Campbell <tonyhawk2100@gmail.com>
2021-06-11 11:14:04 -0700

    continue working on game.

--- src/game.rs
+++ src/game.rs
@@ -3,7 +3,7 @@ use crate::die::Die;
 use crate::scoreboard::Scoreboard;
 
 struct Game {
-    dice: [Die; 5]
+    dice: [Die; 5],
     game_state: GameState,
     scoreboard: Scoreboard
 }
--- src/scoreboard.rs
+++ src/scoreboard.rs
@@ -30,4 +30,30 @@ impl Scoreboard {
             number_of_yahtzee_bonuses: 0
         }
     }
+
+    pub fn upper_score(&self) -> u32 {
+        self.upper_section.values().fold(0, |tally, value| tally + value)
+    }
+
+    pub fn lower_sscore(&self) -> u32 {
+        self.lower_section.values().fold(0, |tally, value| tally + value)
+    }
+
+    pub fn total_score(&self) -> u32 {
+        let mut total: u32 = 0;
+
+        if self.upper_score() >= 63 {
+            total += self.upper_score() + 35;
+        } else {
+            total += self.upper_score();
+        }
+
+        total += self.lower_sscore();
+
+        if self.number_of_yahtzee_bonuses > 0 {
+            total += 100*self.number_of_yahtzee_bonuses;
+        }
+
+        return total;
+    }
 }
