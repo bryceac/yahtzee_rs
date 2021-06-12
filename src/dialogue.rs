097b65735685b51fbfa569a4b4268bc22fa535d3
Bryce Campbell <tonyhawk2100@gmail.com>
2021-06-12 08:35:01 -0700

    continue commenting code.

--- src/dialogue.rs
+++ src/dialogue.rs
@@ -1,12 +1,16 @@
-use text_io::read;
+use text_io::read; // import module to make retrieve data from the user easier
+
+// import stuff needed to convert values and to get things to show better in the CLI.
 use std::{convert::TryInto, io::{stdout, Write} };
 
+/// represents an interactive dialog for the user.
 pub struct Dialogue {
     pub message: String,
     pub choices: Vec<String>
 }
 
 impl Dialogue {
+    /// create dialogue object with a specified prompt and choices.
     pub fn new(message: &str, choices: Vec<String>) -> Dialogue {
         Dialogue {
             message: String::from(message),
@@ -14,9 +18,13 @@ impl Dialogue {
         }
     }
 
+    // activate the dialog and get user selection
     pub fn run(&self) -> u32 {
+
+        // create variable to hold user input. It is an Option, so that it can be used for a loop condition.
         let mut choice: Option<u32> = None;
 
+        // run code unit a proper choice is made. The choice is 1 higher than the index in the vector.
         while let None = choice {
             for (index, choice) in self.choices.iter().enumerate() {
                 println!("{}. {}", index+1, choice);
--- src/die.rs
+++ src/die.rs
@@ -1,13 +1,21 @@
-use rand::Rng;
+use rand::Rng; // import module needed to get random numbers
+
+// import modules necessary to display data and allow comparisons to be made
 use std::{ cmp::{Ordering, PartialEq, PartialOrd}, fmt};
 
+
+/// represents a die object in a game.
 #[derive(Copy, Clone, Eq)]
 pub struct Die {
+    /// value rolled. Defaults to 0.
     pub number: u32,
+
+    /// specifies whether user has it in reserve. Defaults to false.
     pub is_held: bool
 }
 
 impl Die {
+    /// create a new die object.
     pub fn new() -> Die {
         Die {
             number: 0,
@@ -15,11 +23,13 @@ impl Die {
         }
     }
 
+    /// simulate a die roll.
     pub fn roll(&mut self) {
         self.number = rand::thread_rng().gen_range(1..=6);
     }
 }
 
+// help user see information on the die, and denote whether it was held or not.
 impl fmt::Display for Die {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 
@@ -31,12 +41,14 @@ impl fmt::Display for Die {
     }
 }
 
+// implement stuff necessary equality comparisons to be made.
 impl PartialEq for Die {
     fn eq(&self, other: &Self) -> bool {
         self.number == other.number && self.is_held == other.is_held
     }
 }
 
+// implement code needed to make it easy to compare dice.
 impl Ord for Die {
     fn cmp(&self, other: &Self) -> Ordering {
         self.number.cmp(&other.number)
