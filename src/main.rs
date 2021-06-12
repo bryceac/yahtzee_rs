6c993bd9b4ea626884a5a9a2ea691b138a384316
Bryce Campbell <tonyhawk2100@gmail.com>
2021-06-11 17:17:54 -0700

    update new changes.

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,7 @@ use dialogue::Dialogue;
 
 fn main() {
     let mut dice = [Die::new(); 5];
-    let mut numbers: String = String::new();
-
-    let dialog = Dialogue::new("What would you like to do? ", vec![String::from("Roll"), String::from("Quit")]);
-
-    loop {
-        if numbers.is_empty() {} else {
-            println!("{}", numbers);
-        }
-
-        match dialog.run() {
-            1 => {
-                roll(&mut dice);
-                numbers = get_numbers(&dice);
-            },
-            2 => break,
-            _ => {}
-        }
-    }
+    
 }
 
 fn get_numbers(dice: &[Die; 5]) -> String {
