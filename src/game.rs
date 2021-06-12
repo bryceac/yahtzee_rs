4001e13a150f52847850d7b8171c4470fcebc7f5
Bryce Campbell <tonyhawk2100@gmail.com>
2021-06-11 19:40:44 -0700

    make sure user leaves loop after scoring.

--- src/game.rs
+++ src/game.rs
@@ -7,6 +7,7 @@ use crate::dialogue::Dialogue;
 use std::collections::HashMap;
 use maplit::hashmap;
 use count_where::CountWhere;
+use regex::Regex;
 
 struct Game {
     pub dice: [Die; 5],
@@ -23,8 +24,8 @@ impl Game {
         }
     }
 
-    pub fn upper_scoreboard_possibilities(&self, dice: &[Die; 5]) -> HashMap<u32, u32> {
-        let numbers = dice.iter().map(|d| d.number).collect::<Vec<_>>();
+    pub fn upper_scoreboard_possibilities(&self) -> HashMap<u32, u32> {
+        let numbers = self.dice.iter().map(|d| d.number).collect::<Vec<_>>();
 
         hashmap! {
             1 => ((numbers.iter().count_where(|&&n| n == 1)*1) as u32),
@@ -36,7 +37,7 @@ impl Game {
         }
     }
 
-    pub fn lower_scoreboard_possibilities(&self, dice: &[Die; 5]) -> HashMap<String, u32> {
+    pub fn lower_scoreboard_possibilities(&self) -> HashMap<String, u32> {
         let mut possibilities: HashMap<String, u32> = hashmap! {
             String::from("Three of a Kind") => 0,
             String::from("Four of a Kind") => 0,
@@ -47,9 +48,9 @@ impl Game {
             String::from("Chance") => 0
         };
 
-        let sum = dice.iter().fold(0, |tally, d| tally + d.number);
+        let sum = self.dice.iter().fold(0, |tally, d| tally + d.number);
 
-        match Pair::pair(dice) {
+        match Pair::pair(&self.dice) {
             Some(Pair::FiveOfAKind) => {
                 possibilities.insert(String::from("Three of a Kind"), sum);
                 possibilities.insert(String::from("Four of a Kind"), sum);
@@ -68,7 +69,7 @@ impl Game {
             None => ()
         }
 
-        match Combination::combination(dice) {
+        match Combination::combination(&self.dice) {
             Some(Combination::FullHouse) => {
                 possibilities.insert(String::from("Full House"), 25);
             },
@@ -125,8 +126,8 @@ impl Game {
     }
 
     fn score(&mut self) {
-        let upper_possibilities: HashMap<u32, u32> = self.upper_scoreboard_possibilities(&self.dice).iter().filter(|(k, _)| !self.scoreboard.upper_section.contains_key(k)).map(|(&key, &value)| (key, value)).collect();
-        let lower_possibilities: HashMap<String, u32> = self.lower_scoreboard_possibilities(&self.dice).iter().filter(|(k, _)| !self.scoreboard.lower_section.contains_key(k.clone())).map(|(key, &value)| (key.clone(), value)).collect();
+        let upper_possibilities: HashMap<u32, u32> = self.upper_scoreboard_possibilities().iter().filter(|(k, _)| !self.scoreboard.upper_section.contains_key(k)).map(|(&key, &value)| (key, value)).collect();
+        let lower_possibilities: HashMap<String, u32> = self.lower_scoreboard_possibilities().iter().filter(|(k, _)| !self.scoreboard.lower_section.contains_key(k.clone())).map(|(key, &value)| (key.clone(), value)).collect();
 
         let mut choices: Vec<String> = vec![];
 
@@ -141,15 +142,40 @@ impl Game {
         let dialog = Dialogue::new("What do you want to score? ", choices);
 
         loop {
-            let choice = dialog.run();
+            let choice = dialog.run() as usize;
 
             match choice {
                 0 => println!("invalid choice."),
                 _ => {
+                    let selection = dialog.choices[choice-1].clone();
 
+                    if let Some(key) = key_search(&selection) {
+                        if let Ok(number) = key.parse::<u32>() {
+                            if let Some(value) = self.upper_scoreboard_possibilities().get(&number) {
+                                self.scoreboard.upper_section.insert(number, *value);
+                            }
+                        } else {
+                            if let Some(value) = self.lower_scoreboard_possibilities().get(&key) {
+                                self.scoreboard.lower_section.insert(key, *value);
+                            }
+                        }
+                    }
+                    break;
                 }
             }
         }
+    }
+}
+
+fn key_search(text: &String) -> Option<String> {
+    if let Ok(expression) = Regex::new("(.*?) for") {
+        if let Some(captures) = expression.captures(text) {
+            Some(String::from(&captures[1]))
+        } else {
+            None
+        }
+    } else {
+        None
     }
 }
 
