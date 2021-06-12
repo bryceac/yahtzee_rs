c6f4bdeb5e3d78d9e23eba1fa3f2a7ade9223077
Bryce Campbell <tonyhawk2100@gmail.com>
2021-06-11 21:44:21 -0700

    make sure game loop works properly.

--- src/main.rs
+++ src/main.rs
@@ -19,31 +19,32 @@ fn main() {
     loop {
         if let GameState::GameOver = game.game_state { break }
 
-        game.run()
-    }
+        game.run();
 
-    println!("Your final score is {}", game.scoreboard.total_score());
-    println!("Upper section greater than or equal to 63? {}", if game.scoreboard.upper_score_is_63_or_larger() { "Yes" } else { "No" });
-    println!("Multiple Yahtzees? {}", if game.scoreboard.mutliple_yahtzees() { "Yes" } else { "No" });
+        println!("Your final score is {}", game.scoreboard.total_score());
+        println!("Upper section greater than or equal to 63? {}", if game.scoreboard.upper_score_is_63_or_larger() { "Yes" } else { "No" });
+        println!("Multiple Yahtzees? {}", if game.scoreboard.mutliple_yahtzees() { "Yes" } else { "No" });
 
-    let mut play_again: Option<bool> = None;
+        let mut play_again: Option<bool> = None;
 
-    while let None = play_again {
-        print!("Would you like to play again? ");
-        let _ = stdout().flush();
+        while let None = play_again {
+            print!("Would you like to play again? ");
+            let _ = stdout().flush();
 
-        let answer: String = read!();
+            let answer: String = read!();
 
-        match answer.to_lowercase() {
-            _ if answer == String::from("yes") => play_again = Some(true),
-            _ if answer == String::from("no") => play_again = Some(false),
-            _ => println!("Answer can only be yes or no")
+            match answer.to_lowercase() {
+                _ if answer == String::from("yes") => play_again = Some(true),
+                _ if answer == String::from("no") => play_again = Some(false),
+                _ => println!("Answer can only be yes or no")
+            }
+        }
+
+        if play_again.unwrap() {
+            game.scoreboard = Scoreboard::new();
+            game.dice = [Die::new(); 5];
+        } else {
+            game.game_state = GameState::GameOver
         }
     }
-
-    if play_again.unwrap() {
-        game.scoreboard = Scoreboard::new();
-        game.dice = [Die::new(); 5];
-    }
-
 }
