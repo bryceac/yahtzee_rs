ff161225b76ca78b5f537304d8c372c044bbb2e4
Bryce Campbell <tonyhawk2100@gmail.com>
2021-06-13 08:32:51 -0700

    register types with library.

---
+++ src/lib/lib.rs
@@ -0,0 +1,10 @@
+mod combination;
+mod die;
+mod pair;
+mod game_state;
+mod scoreboard;
+
+pub use combination::Combination as Combination;
+pub use pair::Pair as Pair;
+pub use game_state::GameState as GameState;
+pub use scoreboard::Scoreboard as Scoreboard;
